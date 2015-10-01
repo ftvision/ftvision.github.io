15470a83dd6c67c151a485283583b021742216db
feitong.yang@outlook.com <william1121@gmail.com>
2015-10-01 00:00:44 -0400

    updated layout, but no responsive design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,70 @@
 // opt-in fullpage.js
-$(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+// $(document).ready(function() {
+//     $('#fullpage').fullpage();
+// });
 // opt-in tooltip component in bootstrap
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// opt-in fullpage.js; Full initialization
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: false,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: true,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : false,
+        sectionsColor : ['#fff'],
+        paddingTop: '0',
+        paddingBottom: '70px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0, //1024 px,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
