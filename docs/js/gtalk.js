bed2e6a4f70016ccb3afa4396f60640f8baff16c
ftvision <fyang24@jhu.edu>
2020-11-07 11:33:42 -0500

    hugo page

---
+++ docs/js/gtalk.js
@@ -0,0 +1,13 @@
+
+const gitalk = new Gitalk({
+  enable: true,
+  clientID: '0ce4e2c1e0f7341f5349',
+  clientSecret: '26c7618d4cf897dd47ee63c1daae806152516ac3',
+  repo: 'ftvision.github.io',      // The repository of store comments,
+  owner: 'ftvision',
+  admin: ['ftvision'],
+  id: location.pathname,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+})
+
+gitalk.render('gitalk-container')
